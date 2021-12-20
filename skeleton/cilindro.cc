9456c7cef03ba176ecb013a12f20b1ea659a4ff5
Carlos Lopez Martinez <69404571+Electroner@users.noreply.github.com>
2021-12-20 19:00:04 +0100

    Delete skeleton directory

--- skeleton/cilindro.cc
+++
@@ -1,9 +0,0 @@
-#include "cilindro.h"
-
-_Cilindro::_Cilindro(){
-    this->ChangeREV(vector<_vertex3f>{_vertex3f(0,-0.5,0),_vertex3f(0.5,-0.5,0),_vertex3f(0.5,0.5,0),_vertex3f(0,0.5,0)},360);
-}
-
-_Cilindro::_Cilindro(int n){
-    this->ChangeREV(vector<_vertex3f>{_vertex3f(0,-0.5,0),_vertex3f(0.5,-0.5,0),_vertex3f(0.5,0.5,0),_vertex3f(0,0.5,0)},n);
-}
