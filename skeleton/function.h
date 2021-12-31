f790f9e546ecfd2d3fad79d4e11e6667debb62b2
Carlos <clm2k1@gmail.com>
2021-12-31 16:15:01 +0100

    change REVFX to a haack series

--- skeleton/function.h
+++ skeleton/function.h
@@ -3,9 +3,17 @@
 
 #include <math.h>
 
+//EJEMPLO DE USO FUNCION HAACK SERIES NOSE CONE: https://en.wikipedia.org/wiki/Nose_cone_design#Haack_series
+//EJEMPLO DE Von Kármán
+
+const float C_NOSECONE = 0.0;
+const float L_NOSECONE = 1.0;
+const float R_NOSECONE = 0.3;
+
 inline float RevFuncitonFX(float _x)
 {
-    return ((sqrt(abs(_x)+0.25))/cos(_x));
+    float Theta = acos((1-((2*(_x))/(L_NOSECONE))));
+    return ((R_NOSECONE/(sqrt(M_PI)))*(sqrt(Theta-((sin(2*Theta)/2))+(C_NOSECONE*pow(sin(Theta),3)))));
 }
 
 #endif // FUNCTION_H
