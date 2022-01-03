1a5a0f33e2d7a5ea3ba0f937ad671c0acb1d61a4
Carlos <clm2k1@gmail.com>
2022-01-03 22:18:50 +0100

    Added the Arm and make the complete animation of the pivots

--- skeleton/Aarm.cc
+++ skeleton/Aarm.cc
@@ -1,8 +1,10 @@
 #include "Aarm.h"
-
+#include <iostream>
 _Aarm::_Aarm(){
     position_pivot1 = M_PI;
+    position_pivot2 = 0;
     direction_pivot1 = true;
+    direction_pivot2 = true;
 }
 
 _Aarm::~_Aarm(){
@@ -35,27 +37,70 @@ void _Aarm::draw_lighted_flat_shading(){
 void _Aarm::draw_lighted_smooth_shading(){
     Base.draw_lighted_smooth_shading();
     glMatrixMode(GL_MODELVIEW);
+    
     glPushMatrix();
     glTranslatef(cos(position_pivot1)/2, 0, 0);
     glScalef(0.2,0.1,0.2);
     pivot1.draw_lighted_smooth_shading();
     glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(cos(position_pivot1)/2, 0.1, 0);
+    glScalef(0.05,0.2,0.05);
+    lpivot1.draw_lighted_smooth_shading();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0, 0, sin(position_pivot2)/2);
+    glScalef(0.2,0.1,0.2);
+    pivot2.draw_lighted_smooth_shading();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0, 0.1, sin(position_pivot2)/2);
+    glScalef(0.05,0.2,0.05);
+    lpivot2.draw_lighted_smooth_shading();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(0, 0.1, sin(position_pivot2)/2);
+    glRotated(cos(position_pivot1)*90, 0, 1, 0);
+    glTranslated(0,0,0.95);
+    glScalef(0.1,0.1,2);
+    body.draw_lighted_smooth_shading();
+    glPopMatrix();
+
+    cout<<"position_pivot1: "<<position_pivot1<<endl;
+    cout<<"position_pivot2: "<<position_pivot2<<endl;
+    cout<<"pivot1 cos"<< cos(position_pivot1)<<endl;
+    cout<<"pivot2 sin"<< sin(position_pivot2)<<endl;
 }
 
 void _Aarm::draw_texture(){
 
 }
 
-void _Aarm::update(){
+void _Aarm::update(float _step){
     if(direction_pivot1){
-        position_pivot1 -= 0.05;
+        position_pivot1 -= _step;
         if(position_pivot1 >= M_PI){
             direction_pivot1 = false;
         }
     }else{
-        position_pivot1 += 0.05;
+        position_pivot1 += _step;
         if(position_pivot1 <= 0){
             direction_pivot1 = true;
+        }
+    }
+    if(direction_pivot2){
+        position_pivot2 -= _step;
+        if(position_pivot2 >= M_PI){
+            direction_pivot2 = false;
+        }
+    }else{
+        position_pivot2 += _step;
+        if(position_pivot2 <= 0){
+            direction_pivot2 = true;
         }
     }
 }
--- skeleton/Aarm.h
+++ skeleton/Aarm.h
@@ -12,6 +12,8 @@ private:
     _cube body;
     _Cilindro pivot1;
     _Cilindro pivot2;
+    _Cilindro lpivot1;
+    _Cilindro lpivot2;
 
     _Abase Base;
 
@@ -22,6 +24,7 @@ private:
     
 protected:
     bool direction_pivot1;
+    bool direction_pivot2;
 
 public:
     _Aarm();
@@ -34,7 +37,7 @@ public:
     void draw_lighted_flat_shading();
     void draw_lighted_smooth_shading();
     void draw_texture();
-    void update();
+    void update(float _step=0.025);
 };
 
 #endif
